75fd2bd445496b7afeb2c107f6e73cf118051b64
EvgenyRedko <binito1992@mail.ru>
2024-10-02 17:36:14 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,6 +1,9 @@
 def print_params(a = 1, b = 'строка', c = True):
     print(a,b,c)
-values_list =(5,'Привет')
+values_list = (5,'Привет')
 values_list2 = [54.32,'Строка']
 print_params(*values_list)
 print_params(*values_list2)
+dict_ = {5,'Всем привет', 1> 0}
+print_params(*dict_)
+
